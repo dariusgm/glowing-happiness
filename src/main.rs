e0322d98e6438747113b133858a6aa0f8479f93d
Darius Murawski <darius@murawski.blog>
2022-08-27 21:31:15 +0200

    add content match

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,32 @@
 use std::collections::HashMap;
 use std::ffi::OsStr;
 use std::fs;
-use std::fs::File;
-use std::hash::Hash;
 use std::io::Read;
 use std::iter::{Filter, FilterMap};
 use std::path::PathBuf;
+
 use walkdir::{DirEntry, Error, IntoIter, WalkDir};
 
-fn by_names(a: PathBuf, predicates: Vec<&str>) -> Vec<&str> {
-    match a.file_name() {
-        Some(t) => {
-            let mut r = Vec::new();
-            for mut p in predicates {
-                match t.to_str() {
-                    None => {
-                        println!("WARN: Can't convert filepath to str")
-                    }
-                    Some(path) => {
-                        if path.contains(p) {
-                            r.push(p);
-                        }
-                    }
-                }
-            }
-            r
-        }
-        None => Vec::new(),
-    }
-}
-
-fn by_name(a: PathBuf, predicate: &str) -> bool {
-    match a.file_name() {
-        Some(t) => {
-            println!("{:?}", t);
-            true
-        }
-        None => false,
-    }
-}
-
-fn by_content(a: &PathBuf, predicate: &str) -> bool {
+fn by_name(a: &PathBuf, predicate: &str) -> bool {
     match fs::read_to_string(a) {
         Ok(content) => content.contains(predicate),
         Err(_) => false,
     }
+}
+
+fn by_content(a: &PathBuf, predicate: HashMap<&str, &str>) -> Vec<String> {
+    let mut result = Vec::new();
+    match fs::read_to_string(a) {
+        Ok(content) => {
+            for (app, predicate) in predicate.iter() {
+                if content.contains(predicate) {
+                    result.push(String::from(*app));
+                }
+            }
+        }
+        Err(_) => {}
+    }
+    result
 }
 
 fn read_type_content_map() -> HashMap<&'static str, &'static str> {
@@ -74,10 +56,7 @@ fn read_type_name_map() -> HashMap<&'static str, &'static str> {
     ])
 }
 
-fn main() {
-    // let mut type_content_map = read_type_content_map();
-    let mut type_name_map = read_type_name_map();
-    let root = ".";
+fn walk(root: &str) -> Vec<DirEntry> {
     let mut all_files = Vec::new();
     for p in WalkDir::new(root)
         .into_iter()
@@ -86,25 +65,39 @@ fn main() {
     {
         all_files.push(p);
     }
+    all_files
+}
 
-    let mut result = HashMap::<PathBuf, Vec<&str>>::new();
-    for x in all_files {
+fn main() {
+    let type_content_map = read_type_content_map();
+    let type_name_map = read_type_name_map();
+    let root = ".";
+    let files = walk(root);
+
+    let mut result = HashMap::<PathBuf, Vec<String>>::new();
+    for file in files {
+        let key = file.path().to_path_buf();
         for (app, predicate) in type_name_map.iter() {
-            let key = x.path().to_path_buf();
-            if by_content(&key, predicate) {
-                if result.contains_key(&key) {
-                    let mut values = result.get_mut(&key);
-                    match values {
-                        Some(v) => v.push(&app),
-                        _ => {}
-                    };
-                } else {
-                    result.insert(key, vec![&app]);
+            if by_name(&key, predicate) {
+                match result.get_mut(&key) {
+                    Some(v) => v.push(String::from(*app)),
+                    None => {
+                        result.insert(key.clone(), vec![String::from(*app)]);
+                    }
+                };
+            }
+        }
+
+        for app in by_content(&key, type_content_map.clone()) {
+            match result.get_mut(&key) {
+                Some(v) => v.push(app),
+                None => {
+                    result.insert(key.clone(), vec![app]);
                 }
             }
         }
     }
 
-    println!("{:?}", result);
+    println!("{:?}", &result);
     println!("Hello, world!");
 }
